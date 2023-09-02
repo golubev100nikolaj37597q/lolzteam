c719f3504fe4c6bbd43a8b05f341f8c30f9cab77
danyablack <danyablack278@mail.ru>
2023-09-02 13:14:10 +0300

    Сделал основную структуру, начал header

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <img src="./assets/logo.svg" alt="">
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'headerCrypto',
+  props: {
+    
+  }
+}
+</script>
