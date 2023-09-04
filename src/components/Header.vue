024dc34dd94041be8e9661c7e8e6fa7573780aa0
DanielBlack2718 <danyablack278@mail.ru>
2023-09-04 12:14:52 +0300

    Доделал header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,23 @@
 <template>
   <header>
-    <img src="./assets/logo.svg" alt="">
+    <img src="./assets/logo.svg" alt="" class="logo">
+    <div class="line"></div>
+    <div class="search">
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+        <path
+          d="M17.5 17.5L14.5834 14.5833M16.6667 9.58333C16.6667 13.4954 13.4954 16.6667 9.58333 16.6667C5.67132 16.6667 2.5 13.4954 2.5 9.58333C2.5 5.67132 5.67132 2.5 9.58333 2.5C13.4954 2.5 16.6667 5.67132 16.6667 9.58333Z"
+          stroke="#B2BEE2" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
+      </svg>
+      <input type="text" name="" id="" placeholder="Search...">
+
+    </div>
+    <ul class="list flex gap32">
+      <li>Trade</li>
+      <li>P2P</li>
+      <li>Partners</li>
+      <li>Mining</li>
+      <li>Academy</li>
+    </ul>
   </header>
 </template>
 
@@ -8,7 +25,7 @@
 export default {
   name: 'headerCrypto',
   props: {
-    
+
   }
 }
 </script>
