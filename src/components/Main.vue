aa3ba128d8fb386933af51b09a095db3bb6b3ab7
DanielBlack2718 <danyablack278@mail.ru>
2023-09-07 15:42:49 +0300

    Начал делать 1 секцию

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,10 +1,15 @@
 <template>
     <main>
-        <section>
+        <section class="s-1">
             <div class="s1-left-block">
                 <div class="mini-text">
-                    <img src="./assets/img/logo_mini_text.svg" alt="" width="16px">
-                    <img src="./assets/img/mini_text.svg" alt="" width="350px">
+                    <img src="./assets/img/logo_mini_text.svg" alt=""  width="16">
+                    <img src="./assets/img/mini_text.svg" alt="" width="350">
+                </div>
+                <h1>Buy, trade and store cryptocurrencies</h1>
+                <div class="mail">
+                    <img src="./assets/img/mail.svg" alt="" width="22">
+                    <input type="email" placeholder="Email">
                 </div>
             </div>
         </section>
